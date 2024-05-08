048737b7b4674f52de5fd4a940ea5ef167a55446
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2024-05-08 23:08:34 +0200

    Start of network open

---
+++ atari16bit/src/fn_network/network_open.c
@@ -0,0 +1,14 @@
+/**
+ * @brief network open for Atari 16-bit
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "../../../fujinet-network.h"
+
+uint8_t network_open(char* devicespec, uint8_t mode, uint8_t trans)
+{
+
+    return 0;
+}
