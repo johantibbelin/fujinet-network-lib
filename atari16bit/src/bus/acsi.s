793fd89410aad1968e5304c3a678f32ffd2bd8a0
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2024-05-09 15:54:32 +0200

    Started on asm

--- atari16bit/src/bus/acsi.s
+++ atari16bit/src/bus/acsi.s
@@ -1,8 +1,20 @@
-        .export     _acsi_send_command
+        .cpu        m68000
+        .globl     _acsi_send_command
 
 
-; uint8_t sio_read(uint8_t unit, void * buffer, uint16_t len)
+/* uint8_t sio_read(uint8_t unit, void * buffer, uint16_t len)*/
 
 _acsi_send_command:
+    move.l (sp)+,a6
+    move.l (sp)+,a1 /* adr for ACSI command */
+    move.l #txt,-(sp)
+    move.w #9,-(sp)
+    trap #1
+    addq.l #6,sp
+ 
+    move.l a6,-(sp) /* Put return adr back */
+    clr.b d0 /* return 0 */
+    rts
 
-rts
+txt:
+    .asciz "Send ACSI cmd."
