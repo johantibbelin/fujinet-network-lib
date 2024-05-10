cc54a8db80253983df51c83c5c5fafa7da663dd4
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2024-05-10 23:06:41 +0200

    More EQUs dma code started
    Update acsi.s

--- atari16bit/src/bus/acsi.s
+++ atari16bit/src/bus/acsi.s
@@ -1,6 +1,16 @@
-        .cpu        m68000
-        .globl     _acsi_send_command
+    .cpu        m68000
+    .globl     _acsi_send_command
 
+/* EQUs */
+    flock   = $43e
+    dma     = $ff8606
+    data    = $ff8604
+    gpip    = $fffa01
+    hz200   = $4ba
+
+    dma_lo  = $ff8609
+    dma_mid = $ff860b
+    dma_hi  = $ff860d
 
 /* uint8_t sio_read(uint8_t unit, void * buffer, uint16_t len)*/
 
@@ -12,6 +22,18 @@ _acsi_send_command:
     trap #1
     addq.l #6,sp
  
+    move.w #$ff,flock
+
+
+
+    move.w #$190,dma
+    move.w #$090,dma
+
+    move.w #$088,dma
+    move.l #$0009008a,data
+
+
+
     move.l a6,-(sp) /* Put return adr back */
     clr.b d0 /* return 0 */
     rts
